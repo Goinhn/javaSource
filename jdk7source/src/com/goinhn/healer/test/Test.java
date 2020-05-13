7e4f28a8308c3fd5810e9c0fa896e2d529ff10e0
healer <goinhn@163.com>
2020-05-13 21:36:56 +0800

    jdk7

---
+++ jdk7source/src/com/goinhn/healer/test/Test.java
@@ -0,0 +1,22 @@
+package com.goinhn.healer.test;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("bbb");
+
+        Map<String, Integer> map = new HashMap<>();
+
+        Map<String, Integer> mapNow = new ConcurrentHashMap<>();
+
+        BigInteger bigInteger = new BigInteger("10");
+
+    }
+
+}
+
